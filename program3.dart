828d664cec0f854790744b7872f542766948686c
SoftwareDeveloper-flutter <anilamuhammadashraf.software@gmail.com>
2024-09-14 21:53:43 +0500

    program 3 done

---
+++ program3.dart
@@ -0,0 +1,8 @@
+void main(){
+  print('===============Multiplication Table of 5=====================');
+  int number=5;
+  for(var i=1;i<=10;i++){
+      var result=number * i;
+      print("5 x $i =  $result");
+  }
+}
